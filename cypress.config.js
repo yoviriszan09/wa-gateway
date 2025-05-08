5b208a605b4ebce698af75c1db021f4af90263c9
Yovi Riszan <yoviriszan09@gmail.com>
2025-05-08 21:33:47 +0700

    first test

---
+++ cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+const dotenv = require('cypress-dotenv');
+
+module.exports = defineConfig({
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+      config = dotenv(config);
+      return config;
+    },
+  },
+});
---
+++ cypress/e2e/login.cy.js
@@ -0,0 +1,40 @@
+
+
+describe('Login Test', () => {
+    const baseUrl = 'https://wa-gateway.ayocoding.com/auth/login';
+  
+    beforeEach(() => {
+      // Visit the login page before each test
+      cy.visit(baseUrl);
+    });
+  
+    it('should successfully log in with valid credentials', () => {
+      // Replace with valid credentials
+      const phoneNumber = Cypress.env('PHONE_NUMBER') || '6281234567890'; // Use an environment variable or a hardcoded value
+      const password = Cypress.env('PASSWORD') || 'yourpassword'; // Use an environment variable or a hardcoded value
+  
+      // Interact with the login form
+      cy.get(':nth-child(1) > .relative > .w-full').type(phoneNumber); // Adjust the selector if needed
+      cy.get(':nth-child(2) > .relative > .w-full').type(password); // Adjust the selector if needed
+      cy.get('.px-4').click(); // Adjust the selector if needed
+  
+    //   cy.contains('Invalid phoneNumber or password').should('be.visible');
+      // Assert that the user is redirected to the dashboard or another page
+    //   cy.url().should('include', '/dashboard'); // Adjust the URL path as needed
+    //   cy.contains('Welcome').should('be.visible'); // Adjust the text as needed
+    });
+  
+    it('should show an error message for invalid credentials', () => {
+      // Use invalid credentials
+      const phoneNumber = Cypress.env('PHONE_NUMBER') || '6281234567890'; // Use an environment variable or a hardcoded value
+      const password = Cypress.env('PASSWORD') || 'wrongpassword'; // Use an environment variable or a hardcoded value
+  
+      // Interact with the login form
+      cy.get(':nth-child(1) > .relative > .w-full').type(phoneNumber); // Adjust the selector if needed
+      cy.get(':nth-child(2) > .relative > .w-full').type(password); // Adjust the selector if needed
+      cy.get('.px-4').click(); // Adjust the selector if needed
+  
+      // Assert that an error message is displayed
+    //   cy.contains('Invalid phoneNumber or password').should('be.visible'); // Adjust the text as needed
+    });
+  });
